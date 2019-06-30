91b444ea7a23ba92006bff4799816c95dad442c2
= <=>
2019-06-30 18:42:47 +0700

    Bài tập 2.21: (PRIMEFACT) Viết chương trình phân tích một số nguyên dương N < 106 ra thừa số nguyên tố.  Input: N Output: f1,f2,...,fn là các thừa số nguyên tố

---
+++ src/chuong2/Bai2_21Primefact.java
@@ -0,0 +1,22 @@
+package chuong2;
+
+import java.util.Scanner;
+
+public class Bai2_21Primefact {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int n;
+        n = scanner.nextInt();
+        while (n > 1) {
+            for (int i = 2; i <= n; i++) {
+                if (n % i == 0) {
+                    System.out.printf("%d ", i);
+                    n = n / i;
+                    i--;
+                }
+
+            }
+        }
+    }
+
+}
