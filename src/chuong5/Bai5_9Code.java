437dcce5a8c1354d5b25aee3d846bdbd59ab4296
= <=>
2019-07-01 12:37:44 +0700

    Bài tập 5.9: (CODE) Viết chương trình để nén một xâu ký tự theo cách đếm số ký tự giống nhau liên tiếp, nếu 1 ký tự đứng độc lập thì không ghi số 1 ở trước ký tự đó. Input: Chuỗi ký tự st cần nén Output: Chuỗi sau khi nén.

--- src/chuong5/Bai5_9Code.java
+++ src/chuong5/Bai5_9Code.java
@@ -1,4 +1,27 @@
 package chuong5;
 
+import java.util.Scanner;
+
 public class Bai5_9Code {
+    public static String compress(String s) {
+        String out = "";
+        int sum = 1;
+        for (int i = 0; i < s.length() - 1; i++) {
+            if (s.charAt(i) == s.charAt(i + 1)) {
+                sum++;
+            } else {
+                out = out + s.charAt(i) + sum;
+                sum = 1;
+            }
+        }
+        out = out + s.charAt(s.length() - 1) + sum;
+        return out.length() < s.length() ? out : s;
+    }
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Please enter character string : ");
+        String str = scanner.nextLine();
+        System.out.println(compress(str));
+    }
 }
