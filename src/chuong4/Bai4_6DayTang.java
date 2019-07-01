cca6f9a554c44a6523c69154cb7576e01c8ed772
= <=>
2019-07-01 18:10:48 +0700

    Bài tập 4.6: (DAYTANG) Viết chương trình nhập vào một dãy số nguyên a1, a2, ..., an. Tìm độ dài dãy con tăng dần dài nhất. (Dãy con là dãy các phần tử liên tiếp nhau trong mảng). Input: - Dòng đầu chứa số nguyên: N - Dòng tiếp theo chứa các phần tử a1,…,aN Output: S là độ dài dãy con tăng dần dài nhất

--- src/chuong4/Bai4_6DayTang.java
+++ src/chuong4/Bai4_6DayTang.java
@@ -3,39 +3,6 @@ package chuong4;
 import java.util.ArrayList;
 import java.util.Scanner;
 
-
-/*
-private static int mang(ArrayList<Integer> day) {
-    String max = " ";
-    for (int i = 0; i < day.size(); i++) {
-        int dem = 1;
-       for(int j = 0;j<day.size();j++) {
-            if (day.get(j) < day.get(i)) {
-               // F1[i] = max( F[i],F[j]+1)
-
-            } else {
-                i++;
-            }
-
-        }
-        System.out.println(dem);
-
-    }return 0;
-}
-    public static void main (String[]args){
-        ArrayList<Integer> day = new ArrayList<>();
-        Scanner scanner = new Scanner(System.in);
-        int n = scanner.nextInt();
-        for (int i = 0; i < n; i++) {
-            int number = scanner.nextInt();
-            day.add(number);
-        }
-        mang(day);
-        System.out.println(day);
-
-    }
-
- */
 public class Bai4_6DayTang {
     public static void main(String[] args) {
         Scanner scn = new Scanner(System.in);
@@ -49,21 +16,18 @@ public class Bai4_6DayTang {
         }
         int max = 0;
         int[] b = new int[100];
-
         for (int i = 0; i < n; i++) {
             b[i] = 1;
-            // int b = 1;
             for (int j = i; j < n - 1; j++) {
                 if (a.get(j) <= a.get(j + 1)) {
                     b[i]++;
-                    //b++;
                 }
                 if (a.get(j) > a.get(j + 1)) {
                     break;
                 }
             }
             if (b[i] > max) max = b[i];
-            //if (b > max) max = b;
+
         }
         System.out.println("Do dai day con tang dan nhieu nhat: " + max);
     }
