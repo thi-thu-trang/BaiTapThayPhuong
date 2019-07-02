4e35896aa523488701265429dc47a3d6096bec97
= <=>
2019-07-02 21:20:21 +0700

    (SUMS1) S1 = 1 + 1/2 + ... + 1/n

--- src/chuong2/Bai2_11Sum1.java
+++ src/chuong2/Bai2_11Sum1.java
@@ -2,7 +2,7 @@ package chuong2;
 
 import java.util.Scanner;
 
-public class Bai2_11Sum {
+public class Bai2_11Sum1 {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
         double n = scanner.nextInt();
