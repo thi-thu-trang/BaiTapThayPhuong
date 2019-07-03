5dddbf8d1fd198cd053d74d9f1ac3aabe323ec6a
= <=>
2019-07-03 11:40:00 +0700

    (FACT) S0 = n! = 1*2*...*n  {n<23}

--- src/chuong2/Bai2_11Fact.java
+++ src/chuong2/Bai2_11Fact.java
@@ -8,7 +8,7 @@ public class Bai2_11Fact {
         double n = scanner.nextInt();
         double s = 1;
         for (double i = 1; i <= n; i++) {
-            s *= i;
+            s = s * i;
         }
         System.out.printf("%.2f", s);
     }
