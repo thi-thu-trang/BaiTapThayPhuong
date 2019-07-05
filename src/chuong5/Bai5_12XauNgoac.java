2d582d8532492dec99b014cc1cfafad49bd57b17
= <=>
2019-07-05 12:06:38 +0700

    Bài tập 5.12: (XAUNGOAC) Cho xâu ngoặc S có độ dài không quá 100. Hãy xác định xâu S là xâu ngoặc đúng hay sai.  Input: gồm một dòng chứa xâu S. Output: TRUE ứng với xâu S đúng, FALSE nếu S sai.

--- src/chuong5/Bai5_12XauNgoac.java
+++ src/chuong5/Bai5_12XauNgoac.java
@@ -8,16 +8,16 @@ public class Bai5_12XauNgoac {
         do {
             len = s.length();
             s = s.replace("()", "");
-            s = s.replace("{}", "");
-            s = s.replace("[]", "");
+            // s = s.replace("{}", "");
+            // s = s.replace("[]", "");
         } while (len != s.length());
         return s.length() == 0;
     }
 
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
-        String strr = scanner.nextLine();
-        System.out.println(groupCheck(strr));
+        String chuoi = scanner.nextLine();
+        System.out.println(groupCheck(chuoi));
     }
 
 }
