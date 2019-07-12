05716e1b2b848f05c51b0e99eb7f335ba388c089
= <=>
2019-07-12 12:51:27 +0700

    Bài tập 5.4: (REVSTR) Tìm xâu đảo ngược của xâu cho trước. Input: Chuỗi ký tự st Output: Chuỗi đảo ngược của st

---
+++ src/chuong5/Bai5_4Revstr.java
@@ -0,0 +1,16 @@
+import java.util.Scanner;
+
+public class Bai5_4Revstr {
+    public static void main(String[] args) {
+
+        Scanner scanner = new Scanner(System.in);
+        String chuoi = scanner.nextLine();
+        StringBuilder daonguoc = new StringBuilder(chuoi);
+        daonguoc.toString();
+        //chuoi.reverse();
+        //console.log(myArray)
+        System.out.println(scanner);
+
+    }
+}
+
