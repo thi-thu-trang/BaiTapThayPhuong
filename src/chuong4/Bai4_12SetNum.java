3a0f400ae09a9cbb43a1b31407df5d56817220eb
= <=>
2019-07-15 20:35:09 +0700

    Bài tập 4.12: (SETNUM) Cho n số nguyên dương a1, a2,..., an (1 ≤ n, ai ≤ 106). Hãy xác định số lượng các phần tử khác nhau trong dãy số đã cho.  Input:  - Dòng đầu chứa số nguyên dương n. - Dòng sau chứa n số nguyên a1, a2,..., an. Output: một số nguyên S là số các phần tử khác nhau trong dãy số đã cho.

--- src/chuong4/Bai4_12SetNum.java
+++ src/chuong4/Bai4_12SetNum.java
@@ -5,29 +5,23 @@ import java.util.Scanner;
 
 public class Bai4_12SetNum {
     public static void main(String[] args) {
-        Scanner scn = new Scanner(System.in);
-        int n = 0, num;
-        ArrayList<Integer> arr = new ArrayList<>();
-        try {
-            System.out.println("Nhap so phan tu cua mang: ");
-            n = scn.nextInt();
-        } catch (Exception e) {
-            System.out.println(e);
-        }
+
+        ArrayList<Integer> mang = new ArrayList<>();
+        Scanner scanner = new Scanner(System.in);
+        int n = scanner.nextInt();
         for (int i = 0; i < n; i++) {
-            System.out.println("Nhap phan tu thu " + i + ":");
-            num = scn.nextInt();
-            arr.add(num);
+            int number = scanner.nextInt();
+            mang.add(number);
         }
         int dem = 0;
         for (int i = 0; i < n; i++) {
             for (int j = i + 1; j < n; j++) {
-                if (arr.get(i) == arr.get(j)) {
+                if (mang.get(i) == mang.get(j)) {
                     dem++;
                 }
             }
         }
-        int result = arr.size() - dem;
+        int result = mang.size() - dem;
         System.out.println("So cac phan tu khac nhau: " + result);
     }
 }
