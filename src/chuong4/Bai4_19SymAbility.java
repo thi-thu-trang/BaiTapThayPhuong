48ebbe745fcd7e2bbce68880e96837d16eeba66e
= <=>
2019-07-16 23:21:26 +0700

    Bài tập 4.19: (SYMABILITY) Dãy số nguyên được gọi là dãy khả đối xứng nếu có thể sắp xếp lại thành một dãy đối xứng. Cho dãy số nguyên gồm n phần tử (n>0). Hãy kiểm tra dãy có phải là khả đối xứng? Input:   - Dòng đầu: n (n<106).  - Dòng sau ghi n số nguyên a1, a2,...,an (ai<1018).  Output: TRUE/FALSE tương ứng với khả đối xứng

---
+++ src/chuong4/Bai4_19SymAbility.java
@@ -0,0 +1,36 @@
+package chuong4;
+
+import java.util.ArrayList;
+import java.util.Scanner;
+
+public class Bai4_19SymAbility {
+    private static boolean Check(int n, ArrayList<Integer> arr) {
+        for (int i = 0; i < n; i++) {
+            for (int j = n - 1; j > 0; j--) {
+                if (arr.get(j).equals(arr.get(j))) {
+                    return true;
+                } else return false;
+            }
+        }
+        return false;
+    }
+
+    public static void main(String[] args) {
+        Scanner scn = new Scanner(System.in);
+        int n = 0, num;
+        ArrayList<Integer> arr = new ArrayList<>();
+        try {
+            System.out.println("Nhap so phan tu cua mang: ");
+            n = scn.nextInt();
+        } catch (Exception e) {
+            System.out.println(e);
+        }
+        for (int i = 0; i < n; i++) {
+            System.out.println("Nhap phan tu thu " + i + ":");
+            num = scn.nextInt();
+            arr.add(num);
+        }
+        Check(n, arr);
+        System.out.println(Check(n, arr));
+    }
+}
